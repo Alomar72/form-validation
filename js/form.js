5b27b38b9396a13a9c28b35dd0f8b93533440e56
OlgitH <berman.oj@gmail.com>
2022-01-26 13:46:00 +0000

    ~

--- js/form.js
+++ js/form.js
@@ -1,49 +1,10 @@
 
+const errorsDiv = document.getElementById('errors');
+
 function submitHandler(e){
 
-  const errorsDiv = document.getElementById('errors');
-  let errors = '';
-
-  let validate = function() {
-    const fname = document.getElementById('fname');
-    const sname = document.getElementById('sname');
-    const mobileNumber = document.getElementById('mobileNumber');
-    const homeNumber = document.getElementById('homeNumber');
-    const comment = document.getElementById('comment');
-    const rating = document.getElementById('rating');
-
-
-    const notLettersOrSpaces = /[^a-zA-Z ]/g;
-    const notNumbersOrSpaces = /[^0-9 ]/g;
-
-    if (fname.value.match(notLettersOrSpaces)) {
-      // console.log('Please only use letters or spaces');
-      fname.classList.add('error');
-      // console.log(fname.value);
-      errors += 'Please only use letters or spaces' + '<br>';
-    }
-    if (fname.value == "") {
-      fname.classList.add('error');
-      errors += 'fname needs filling' + '<br>';
-    }
-    if (sname.value == "") {
-      sname.classList.add('error');
-      errors += 'sname needs filling' + '<br>';
-    }
-    if (mobileNumber.value.match(notLettersOrSpaces)) {
-      // console.log('Please only use letters or spaces');
-      mobileNumber.classList.add('error');
-      // console.log(fname.value);
-      errors += 'Please only use letters or spaces' + '<br>';
-    }
-    if (mobileNumber.value == "") {
-      mobileNumber.classList.add('error');
-      errors += 'sname needs filling' + '<br>';
-    }
-
-  }
-
   validate();
+
   if (errors) {
     errorsDiv.innerHTML = errors;
   } else {
